268b4b0f65c021e979d71c676c333644cc611457
Ben Espey <bennytytyty@gmail.com>
2016-02-01 10:31:54 -0500

    Add symlinking shell script

---
+++ symlinker.sh
@@ -0,0 +1,5 @@
+ln -s ~/.vim/.vimrc ~/.vimrc
+ln -s ~/.vim/.gvimrc ~/.gvimrc
+ln -s ~/.vim/.zshrc ~/.zshrc
+ln -s ~/.vim/ben.zsh-theme ~/.oh-my-zsh/themes/ben.zsh-theme
+ln -s ~/.vim/.aliases ~/.aliases
