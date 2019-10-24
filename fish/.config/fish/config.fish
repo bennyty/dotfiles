4e6b79c88e48b9f21d23cca9837511bd02b8e5e3
bennyty <bennytytyty@gmail.com>
2019-10-24 14:14:48 -0400

    Fish config changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,9 +1,18 @@
 fish_hybrid_key_bindings
 
+set -x EDITOR /usr/local/bin/nvim
+set -x LESS "rj.5"
+
 alias ls=exa
 alias la="exa -a"
 alias ll="exa --all --long --git"
 alias lst2='exa -l -T -L 2 --header --git-ignore -F -d -B --color-scale'
-alias ef='eval $EDITOR "~/.config/fish/config.fish"'
+alias ef='$EDITOR ~/.config/fish/config.fish'
 alias vim='nvim'
 alias gst='git status'
+alias dirs='cdh'
+alias dip="docker inspect -f '{{.NetworkSettings.IPAddress}}'"
+
+bind -M insert \cd expand_glob
+
+source ~/.iterm2_shell_integration.(basename $SHELL)
